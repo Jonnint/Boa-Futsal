f32f55faeb4bb41dd585ea2d10adfd8638fa5426
zshnrdtya <radityaraizeeshan@gmail.com>
2026-09-13 00:12:58 +0700

    feat(view-composer): buat ActiveSportTypeComposer untuk inject sport type aktif ke view

---
+++ boafutsalv1/app/Http/ViewComposers/ActiveSportTypeComposer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Cache;
+use App\Models\SportType;
+
+class ActiveSportTypeComposer
+{
+    public function compose(View $view): void
+    {
+        $activeSportType = Cache::rememberForever('active_sport_type', function () {
+            return SportType::with(['galleries', 'fields.prices'])
+                ->where('is_active', true)
+                ->first();
+        });
+
+        $view->with('activeSportType', $activeSportType);
+    }
+}
