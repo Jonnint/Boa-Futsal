80e38acc361c56c411fd62f8f639fe06760a2598
zshnrdtya <radityaraizeeshan@gmail.com>
2026-09-13 00:13:03 +0700

    seed: daftarkan DeveloperSeeder dan SportTypeSeeder ke DatabaseSeeder

--- boafutsalv1/database/seeders/DatabaseSeeder.php
+++ boafutsalv1/database/seeders/DatabaseSeeder.php
@@ -18,7 +18,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             FieldSeeder::class,
             AdminSeeder::class,
+            DeveloperSeeder::class,
             ChatbotSettingSeeder::class,
+            SportTypeSeeder::class,
         ]);
     }
 }
