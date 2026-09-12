a1b6fb89445e1f4bf6a25c0ff55a0fb533cfdd3e
zshnrdtya <radityaraizeeshan@gmail.com>
2026-09-13 00:13:09 +0700

    ui(sejarah): sesuaikan judul halaman sejarah dengan jenis olahraga aktif

--- boafutsalv1/resources/views/sejarah.blade.php
+++ boafutsalv1/resources/views/sejarah.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sejarah - BOA Futsal</title>
+    <title>Sejarah - BOA {{ strtoupper($activeSportType->name ?? 'Futsal') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
