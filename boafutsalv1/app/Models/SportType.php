4d36518f4cf2adbe4a71c8733951e7e7aa5d9b3a
zshnrdtya <radityaraizeeshan@gmail.com>
2026-09-13 00:12:50 +0700

    feat(model): tambahkan model SportType untuk manajemen jenis olahraga

---
+++ boafutsalv1/app/Models/SportType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class SportType extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'hero_title',
+        'hero_subtitle',
+        'description',
+        'hero_image_path',
+        'facilities',
+        'meta_title',
+        'meta_description',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'facilities' => 'array',
+    ];
+
+    public function fields(): HasMany
+    {
+        return $this->hasMany(Field::class, 'sport_type_id', 'id');
+    }
+
+    public function galleries(): HasMany
+    {
+        return $this->hasMany(SportTypeGallery::class, 'sport_type_id', 'id')
+            ->whereNull('archived_at')
+            ->orderBy('order');
+    }
+
+    public function archivedGalleries(): HasMany
+    {
+        return $this->hasMany(SportTypeGallery::class, 'sport_type_id', 'id')
+            ->whereNotNull('archived_at');
+    }
+
+    public function switchLogs(): HasMany
+    {
+        return $this->hasMany(SportTypeSwitchLog::class, 'to_sport_type_id', 'id');
+    }
+}
