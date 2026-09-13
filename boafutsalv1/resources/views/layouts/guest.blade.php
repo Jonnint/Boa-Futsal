10fc85d4ffb64552f49ea362d02289858a9f2991
zshnrdtya <radityaraizeeshan@gmail.com>
2026-09-13 20:30:33 +0700

    views: perbarui title tag pada layout guest mengikuti cabang olahraga aktif

--- boafutsalv1/resources/views/layouts/guest.blade.php
+++ boafutsalv1/resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ $activeSportType ? 'BOA ' . strtoupper($activeSportType->name) : config('app.name', 'Laravel') }}</title>
         <link rel="icon" type="image/jpeg" href="{{ asset('asset/img/favicon.jpg') }}">
 
         <!-- Fonts -->
